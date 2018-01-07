61fdd0f30f537a007a1be839bb4be88d403332bc
Stefan Becker <ich@funbaker.de>
2018-01-07 14:48:17 +0100

    switch to pandas

--- account_bank_statement_import_gls/models/account_bank_statement_import_gls.py
+++ account_bank_statement_import_gls/models/account_bank_statement_import_gls.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+from io import BytesIO
 
-from openerp import api, fields, models, _, tools
-
-import csv
-import StringIO
-from datetime import datetime
-import operator
-import codecs
-import hashlib
+from openerp import models
 
 HEADERS = [
     u'Kontonummer',
@@ -34,201 +29,49 @@ HEADERS = [
     u'Währung'
 ]
 
-def iso8859_utf8(buf):
-    return unicode(buf.decode("iso-8859-1").encode("utf8"), "utf8")
-
-def iso8859_proxy(reader):
-    #TODO: more performance by converting k only once
-    for row in reader:
-        ret = {iso8859_utf8(k): iso8859_utf8(v) for k, v in row.items()}
-        yield ret
-
-def monetize(s):
-    r = s.replace(".", "").replace(",", ".")
-    return float(r)
 
 class AccountBankStatementImport(models.TransientModel):
     _inherit = 'account.bank.statement.import'
 
-    @property
-    def notifications(self):
-        if getattr(self, "_notifications", None) is None:
-            self._notifications = []
-        return self._notifications
-
     def _parse_file(self, data_file):
-        account_invoice = self.env["account.invoice"].search(
-            [('reference', '!=', False)])
-
         try:
-            reader = csv.DictReader(
-                StringIO.StringIO(data_file), delimiter=";")
-            fieldnames = map(
-                lambda x: iso8859_utf8(x),
-                reader.fieldnames
-            )
-            if fieldnames != HEADERS:
-                raise ValueError("Fieldnames differ from GLS Standard")
+            datafile = BytesIO(data_file)
+            dataset = pd.read_csv(
+                datafile,
+                encoding='iso-8859-1',
+                sep=';', quotechar='"',
+                parse_dates=['Buchungstag'], dayfirst=True,
+                decimal=',', thousands='.',
+            ).dropna(how='all')
 
-            self.currency_code = "EUR"
-            self.account_number = ""
+            currency_codes = set(dataset[u'Währung'])
 
-            # this holds the statements to be imported into odoo
-            self.bank_statement_data = []
+            if len(currency_codes) > 1:
+                raise ValueError()
 
-            # this holds the statement lines
-            self.transactions = []
+            if list(dataset.keys()) != HEADERS:
+                raise ValueError()
 
-            # this holds data which will not be imported,
-            # but needed for computation
-            self.meta = []
+            currency_code = next(iter(currency_codes))
+            account_number = None
 
-            for row in iso8859_proxy(reader):
-                row = Row(row) # your boat, gently down the stream
+            rows = dataset[::-1].iterrows()
 
-                # make new statement when account number changes
-                if all([
-                    len(self.account_number) > 0,
-                    row.raw["Kontonummer"] != self.account_number
-                ]):
-                    self._append_bank_statement()
-                    return (
-                        self.currency_code,
-                        self.account_number,
-                        self.bank_statement_data
-                    )
+            bank_statement_data = [{
+                "date": max(dataset['Buchungstag']).strftime('%Y-%m-%d'),
+                "transactions": [{
+                    "name": row["Buchungstext"],
+                    "date": row["Buchungstag"].strftime('%Y-%m-%d'),
+                    "amount": row["Betrag"],
+                    "partner_name": row[u"Auftraggeber/Empfänger"]
+                } for _, row in rows]
+            }]
 
-                self.account_number = row.raw["Kontonummer"]
-
-                self.transactions.append(row.items())
-
-                for i in account_invoice:
-                    if i.reference in row.ref:
-                        self.transactions[-1]["name"] = i.reference
-                        break
-                    if i.number in row.ref:
-                        self.transactions[-1]["name"] = i.number
-                        break
-
-                    sale_order = self.env["sale.order"].search([
-                        ('invoice_ids', 'in', [i.id])
-                    ])
-                    for i_i in sale_order:
-                        if i_i.name in row.ref:
-                            self.transactions[-1]["name"] = i_i.name
-                            break
-
-                meta = {}
-                # set Kontostand to 0, when its empty
-                try:
-                    meta["Kontostand"] = monetize(row.raw["Kontostand"])
-                except ValueError:
-                    meta["Kontostand"] = 0.0
-
-                self.meta.append({
-                    "Kontostand": meta["Kontostand"],
-                    "Betrag": row.amount
-                })
-
-            self._append_bank_statement()
             return (
-                self.currency_code,
-                self.account_number,
-                self.bank_statement_data
+                currency_code,
+                account_number,
+                bank_statement_data
             )
-        except ValueError, e:
+        except (ValueError, KeyError):
             return super(
                 AccountBankStatementImport, self)._parse_file(data_file)
-
-    def _append_bank_statement(self):
-        """
-        to be called after Kontostand and Betrag are in self.meta
-        """
-        bsd = {
-            # csv is ordered top down wtf
-            # get the newest date in transactions
-            "date": max(map(lambda x: x["date"], self.transactions)),
-            "balance_start": reduce(operator.add, (
-                self.meta[-1]["Kontostand"],
-                self.meta[-1]["Betrag"]
-            )),
-            # balance end is the last Kontostand
-            "balance_end_real": self.meta[0]["Kontostand"],
-            "transactions": self.transactions
-        }
-
-        abs = self.env["account.bank.statement"].search([],
-            order = "date desc", limit = 1)
-
-        if len(abs) and tools.float_compare(
-            abs.mapped("balance_end_real")[0],
-            bsd["balance_start"],
-            precision_digits = 2
-        ) != 0:
-            self.notifications.append({
-                "type": "warning",
-                "message": _("Bank Statements are discontinous")
-            })
-
-        self.bank_statement_data.append(bsd)
-
-        self.transactions = []
-        self.meta = []
-
-    def _create_bank_statements(self, stmts_vals):
-        statement_ids, notifications = super(
-            AccountBankStatementImport, self)._create_bank_statements(
-                stmts_vals)
-
-        notifications += self.notifications
-
-        return statement_ids, notifications
-
-
-class Row(object):
-    def __init__(self, row):
-        self._row = row
-
-    @property
-    def amount(self):
-        return monetize(self._row["Betrag"])
-
-    @property
-    def date(self):
-        return datetime.strptime(
-            self._row["Buchungstag"], "%d.%m.%Y").strftime(
-                "%Y-%m-%d")
-
-    @property
-    def ref(self):
-        return " ".join((
-            # VWZ1-14
-            self._row["VWZ{0}".format(i + 1)] for i in range(14)
-        ))
-
-    @property
-    def name(self):
-        return self._row["Buchungstext"]
-
-    @property
-    def partner_name(self):
-        return self._row[u"Auftraggeber/Empfänger"]
-
-    @property
-    def raw(self):
-        return self._row
-
-    @property
-    def unique_import_id(self):
-        m = hashlib.md5()
-        m.update(self.ref)
-        return m.digest()
-
-    def items(self):
-        return {
-            "amount": self.amount,
-            "date": self.date,
-            "ref": self.ref,
-            "name": self.name,
-            "partner_name": self.partner_name
-        }
